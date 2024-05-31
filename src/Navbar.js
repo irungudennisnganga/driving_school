5c78fa0b6f7bcd8ce30f8de23f0d768e89cdb4b3
+ <+>
2024-06-01 00:27:11 +0300

    fixed navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,18 @@
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-    return (
-      <nav className="bg-gray-800 p-4">
-        <ul className="flex justify-between">
-          <li className="mr-6">
-            <Link to="/" className="text-white hover:text-gray-400">Home</Link>
-          </li>
-          <li>
-            <Link to="/display" className="text-white hover:text-gray-400">Display Data</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav className="bg-gray-800 p-4 fixed top-0 z-50 w-full">
+      <ul className="flex justify-between">
+        <li className="mr-6">
+          <Link to="/" className="text-white hover:text-gray-400">Home</Link>
+        </li>
+        <li>
+          <Link to="/display" className="text-white hover:text-gray-400">Display Data</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
-  export default Navbar
+export default Navbar;
